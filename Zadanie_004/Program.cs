0b4ca0dc5b15a23c4a20f740b11608befd676c38
Philipp K <philippkargapoloff@gmail.com>
2022-10-22 20:52:22 +0300

    added zadanie 004

---
+++ Zadanie_004/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if(Square(num1, num2))
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
+
+bool Square(int num1, int num2)
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;
+}
+
