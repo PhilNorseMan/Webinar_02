a480b3194e9b043b4ab791e3b947aabd8506fb96
Philipp K <philippkargapoloff@gmail.com>
2022-10-22 19:17:16 +0300

    added zadanie 001

---
+++ Zadanie_002/Program.cs
@@ -0,0 +1,21 @@
+﻿void GetDivisionMultiplicity(int num1, int num2)
+{
+    int result = num1 % num2;
+
+    if(result == 0)
+    {
+        Console.WriteLine("Числа являются кратными");
+    }
+    else
+    {
+        Console.WriteLine("Числа не являются кратными. Остаток = " + result);
+    }
+
+}
+
+Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine()!);
+
+GetDivisionMultiplicity(num1, num2);
