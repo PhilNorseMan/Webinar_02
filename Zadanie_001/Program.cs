3aa199090eb2266a79208d4124eadb282515f161
Philipp K <philippkargapoloff@gmail.com>
2022-10-22 13:39:26 +0300

    added zadanie_001

---
+++ Zadanie_001/Program.cs
@@ -0,0 +1,17 @@
+﻿int GenerateRandomNumber()
+{
+    return new Random().Next(100, 1000);
+}
+
+int randomNumber = GenerateRandomNumber();
+Console.WriteLine("Сгенерированное число = " + randomNumber);
+
+int firstNum = randomNumber / 100;
+//int secondNum = randomNumber / 10 % 10;
+int thirdNum = randomNumber % 100 % 10;
+
+//Console.WriteLine("первая цифра = " + firstNum);
+//Console.WriteLine("вторая цифра = " + secondNum);
+//Console.WriteLine("третья цифра = " + thirdNum);
+
+Console.WriteLine("Число без второй цифры = " + firstNum + thirdNum);
