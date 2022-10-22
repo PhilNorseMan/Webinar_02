59a2c1e56d9b3f512bda114beb0cfd60b8f7034e
Philipp K <philippkargapoloff@gmail.com>
2022-10-22 19:29:57 +0300

    added zadanie 003

---
+++ Zadanie_003/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine()!);
+
+if(Mult(num))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+bool Mult(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
